8fc5ed4c419dd9a31697164976dc33bbfe04847b
thegamedenerd <gamedenerd@hotmail.com>
2022-02-27 17:58:02 -0300

    final-update

--- js/menuToggle.js
+++ js/menuToggle.js
@@ -1,8 +1,11 @@
 const menuMobile = document.getElementById('menuHamb')
 
 
-menuMobile.addEventListener('click', function(){
-    alert('teste')
+menuMobile.addEventListener('click',function(){
+    
+    const menuMobileCont = document.querySelector('.menuMobileCont')
+
+    menuMobileCont.classList.toggle('active')
 })
 
 export {menuMobile}
